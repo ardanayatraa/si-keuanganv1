88b487b6caa7d249256121978b29cb05a3f1ea32
Ardana Yatra <imadeardanayatra@gmail.com>
2025-06-13 14:04:00 +0800

    WIP google

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -121,12 +121,6 @@
                                 'url' => '/laporan',
                                 'route_pattern' => 'laporan*',
                             ],
-                            [
-                                'name' => 'Admin',
-                                'icon' => 'M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z',
-                                'url' => '/admin',
-                                'route_pattern' => 'admin*',
-                            ],
                         ];
                     @endphp
 
